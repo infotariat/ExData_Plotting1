2ceccbe34034ad4557c3de8b9ea27a6fe5a10ebc
Barry Snowe <barrysnowe@gmail.com>
2014-08-10 12:28:59 -0700

    plot3.R code

---
+++ plot3.R
@@ -0,0 +1,12 @@
+# plot3.R
+
+plot(newtimeunits, house$subMet1, type ="l", col = "purple", 
+     ylab = "Energy sub metering", xlab = NA)
+lines (newtimeunits, house$subMet2, col="red")
+lines (newtimeunits, house$subMet3, col = "blue")
+
+legend ("topright", pch = "_", col = c("purple", "red", "blue"),
+        legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+dev.copy(png, file = "plot3.png", width = 480, height = 480) 
+dev.off() # don't forget to do this!
