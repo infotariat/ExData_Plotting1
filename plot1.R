65967655ada268cab14ec459a4746d495a6647b5
Barry Snowe <barrysnowe@gmail.com>
2014-08-09 17:04:37 -0700

    I have added different versions of the plot1.r and plot1.png files

--- plot1.R
+++ plot1.R
@@ -1,20 +1,6 @@
-# code for chart #1
-# global active power histogram
+# plot1.R
 
-#import data
-zap <- read.table("household_power_consumption.txt", header = TRUE, 
-                  sep = ';')
-
-global <- as.numeric(zap[66637:69516,3])
-divbythou<-function(x){
-        result<-x/1000
-}
-divbythou(global)
-
-with(zap, hist(divbythou(global),xlim=c(0,6), ylim=c(0,1200), col="red", 
-               main = "Global Active Power", xlab="Global Active Power (kilowatts)"))
-
-     
-  
-
-
+# this code assumes you have used another file of code to download 
+# the data set  
+hist(house$globAct, col="red", main = "Global Active Power", 
+     xlab="Global Active Power (kilowatts)")
