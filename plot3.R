b0fef1b815d81581a80cf00a8ce435ef2dc3f4ed
Barry Snowe <barrysnowe@gmail.com>
2014-08-10 12:49:33 -0700

    made a change to the legend parameters

--- plot3.R
+++ plot3.R
@@ -5,7 +5,7 @@ plot(newtimeunits, house$subMet1, type ="l", col = "purple",
 lines (newtimeunits, house$subMet2, col="red")
 lines (newtimeunits, house$subMet3, col = "blue")
 
-legend ("topright", pch = "_", col = c("purple", "red", "blue"),
+legend ("topright", pch = "_", text.width = 60000,col = c("purple", "red", "blue"),
         legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
 dev.copy(png, file = "plot3.png", width = 480, height = 480) 
