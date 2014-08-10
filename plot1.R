7945c4593a52f29168be7afaaa59b4684d3655bd
Barry Snowe <barrysnowe@gmail.com>
2014-08-10 12:24:37 -0700

    new commit of plot1 code

--- plot1.R
+++ plot1.R
@@ -4,3 +4,8 @@
 # the data set  
 hist(house$globAct, col="red", main = "Global Active Power", 
      xlab="Global Active Power (kilowatts)")
+
+
+# create the png file
+dev.copy(png, file = "plot1.png", width = 480, height = 480) 
+dev.off() # don't forget to do this!
